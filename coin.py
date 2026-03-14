29fafa1c1b571f98e1ce8dcc8a15b4aa18638405
Ryma Djoudad <zenyod85@gmail.com>
2026-03-14 13:44:47 -0400

    Added constructor and toss method

--- coin.py
+++ coin.py
@@ -5,3 +5,15 @@ Class representing tossable coin that lands on either heads or tails
 No starter code 
 03/14/2026
 """
+import random
+class coin:
+
+    def __init__(self):
+        self.__sideup = "Heads"
+
+    def toss(self):
+        toss_decison = random.randint(0, 1)
+        if toss_decison == 0:
+            self.__sideup = "Heads"
+        elif toss_decison == 1:
+            self.__sideup = "Tails"
