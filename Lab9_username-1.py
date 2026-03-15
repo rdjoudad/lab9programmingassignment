2c7e44eb4a3cc178a270d52866a439d0776a46ca
Ryma Djoudad <zenyod85@gmail.com>
2026-03-15 11:06:40 -0400

    Wrapping up project and testing

--- Lab9_username-1.py
+++ Lab9_username-1.py
@@ -39,11 +39,18 @@ def main():
         print(f"Player 1 has {player1.get_wallet()} coins.")
         print(f"Player 2 has {player2.get_wallet()} coins.")
     
-    answer = input("Do you want to toss the coins? (y/n) ")
+        answer = input("Do you want to toss the coins? (y/n) ")
 
     print("---Final Score---")
     print(f"Player 1: {player1.get_wallet()}")
     print(f"Player 2: {player2.get_wallet()}")
+
+    if player1.get_wallet() > player2.get_wallet():
+        print("Player 1 wins!")
+    elif player1.get_wallet() < player2.get_wallet():
+        print("Player 2 wins!")
+    elif player1.get_wallet() == player2.get_wallet():
+        print("It's a draw!")
 
     
 
